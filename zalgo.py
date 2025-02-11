274b54c951e9500ca9a47bb6c1e9061283f8b28d
Seraphania <155034998+Seraphania@users.noreply.github.com>
2025-02-11 18:02:30 +0800

    Create zalgo.py
    Make it into an app with a GUI one day.

---
+++ zalgo.py
@@ -0,0 +1,47 @@
+import random
+import os
+
+def check_in(num):
+    while True:
+        try:
+            num = int(num)
+            if 1 <= num <= 32:
+                return num
+            else:
+                num = input("Please enter a whole number from 1-32:\n")
+        except:
+            ValueError
+            num = (input("Please enter a whole number from 1-32:\n"))
+
+def mess_up(string, madness):
+
+    marks = list(map(chr, range(768, 879)))
+    new_string = ""
+    words = string.split()
+    for word in words:
+        new_string += ' '
+        for letter in word:
+            new_string += letter + "".join(random.choices(marks, k = madness))
+    return new_string
+
+def run_loop(inp):
+    if inp == "y":
+        string = input("What text would you like messed up?:\n")
+        madness = check_in(input("On a scale of 1-32, how messed up would you like it to be?\n"))
+        os.system('clear')
+        os.system('cls')
+        print(mess_up(string, madness))
+        run_loop(input("Press 'y' to mess up more text, type 'ahhhh' to exit:\n"))
+        os.system('clear')
+        os.system('cls')
+    elif inp == "ahhhh":
+        os.system('clear')
+        os.system('cls')
+        print("Thank you for messing up your text")
+        exit()
+    else:
+        os.system('clear')
+        os.system('cls')
+        run_loop(input("would you like to exit? y/ahhhh?:\n"))
+
+run_loop("y")
